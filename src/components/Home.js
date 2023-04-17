708c97788a4ff273c2aec440a46514e49e02111a
Kao Saelor <saelor.kao@gmail.com>
2023-04-17 00:47:50 -0700

    contact me form

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Projects from "./Projects";
 import Skills from "./Skills";
 import Experience from "./Experience";
-import Contact from "./Contact";
 
 class Home extends Component {
   render() {
@@ -17,10 +16,6 @@ class Home extends Component {
           resumeBasicInfo={this.props.resumeData.basic_info}
         />
         <Experience
-          resumeExperience={this.props.resumeData.experience}
-          resumeBasicInfo={this.props.resumeData.basic_info}
-        />
-          <Contact
           resumeExperience={this.props.resumeData.experience}
           resumeBasicInfo={this.props.resumeData.basic_info}
         />
